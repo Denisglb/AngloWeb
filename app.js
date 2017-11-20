fe613a8fea1381a0d09ed0a8b31a53b0475a2310
Denis <denisglb@yahoo.co.uk>
2017-11-20 17:01:22 +0000

    Finished page Philosophy and added Guardianship

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
 const express = require('express');
-const fs = require('fs');
 
 var app = express();
 
-app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/views'));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
 
+app.set('view engine', 'ejs');
 
+app.get('/', function (req, res)
+{
+    res.render('why.html');
+});
 
 app.get('/', (req, res) => {
-	fs.readFile('/views/home.html',null, function(error, data){
-		if (error) {
-			response.writeHead(404);
-			response.write('File not Found');
-
-		} else {
-			response.write(data);
-		}
-		response.end();
-	});
-	response.end();
+	res.render('home.html')
 })
 
 // /bad - send back Json with Error Message
