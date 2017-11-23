decb0f105ef6e251ccb5ca459b118478dbb8646b
Denis <denisglb@yahoo.co.uk>
2017-11-23 13:45:07 +0000

    Testing navbar features

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
 const app = express();
 
-const path = require("path")
+const path = require("path");
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
+
 
 app.use(express.static(path.join(__dirname, 'views')));
 
@@ -12,10 +14,5 @@ app.get('/', function (req, res)
     res.sendFile(path.join(__dirname+'/views/home.html'))
 });
 
-app.get('/', (req, res) => {
-	res.render('home.html')
-})
 
-// /bad - send back Json with Error Message
-
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 3000)
